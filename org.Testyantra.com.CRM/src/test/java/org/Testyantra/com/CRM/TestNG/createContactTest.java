a4fc050e4ae81f4aa9094cab131096553cefdc76
Aman <Aman@hp>
2022-03-23 17:16:25 +0530

    My 1st commit with createContact.

---
+++ org.Testyantra.com.CRM/src/test/java/org/Testyantra/com/CRM/TestNG/createContactTest.java
@@ -0,0 +1,13 @@
+package org.Testyantra.com.CRM.TestNG;
+
+import org.testng.annotations.Test;
+
+public class createContactTest {
+
+	
+	@Test
+	public void createContact()
+	{
+		System.out.println("contact  created");
+	}
+}
