cb7004f9d5cf864aa9d60a96fafa36e198daeebc
Aman <Aman@hp>
2022-03-24 11:58:54 +0530

    2nd modification

---
+++ org.Testyantra.com.CRM/src/test/java/org/Testyantra/com/CRM/TestNG/CreateInvoiceTest.java
@@ -0,0 +1,12 @@
+package org.Testyantra.com.CRM.TestNG;
+
+import org.testng.annotations.Test;
+
+public class CreateInvoiceTest {
+	
+	@Test
+	public void createInvoice()
+	{
+		System.out.println("User 1 created invoice");
+	}
+}
