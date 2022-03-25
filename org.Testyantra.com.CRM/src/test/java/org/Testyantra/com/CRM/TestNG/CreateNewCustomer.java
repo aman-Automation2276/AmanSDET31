e070f86272e6ba3a1e0ebdf40bb12a403ff050f1
Aman <Aman@hp>
2022-03-25 08:28:34 +0530

    3rd commit with cutomer added

---
+++ org.Testyantra.com.CRM/src/test/java/org/Testyantra/com/CRM/TestNG/CreateNewCustomer.java
@@ -0,0 +1,12 @@
+package org.Testyantra.com.CRM.TestNG;
+
+import org.testng.annotations.Test;
+
+public class CreateNewCustomer {
+
+	@Test
+	public void addCutomer()
+	{
+		System.out.println("New Cutomer Added");
+	}
+}
